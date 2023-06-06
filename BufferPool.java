84ebc8bd69c9acc23239d59b8acbb26e4e1fe954
Pranav Prabhu <ppranavas@gmail.com>
2023-06-06 18:13:56 -0400

    Update Version 1

---
+++ BufferPool.java
@@ -0,0 +1,19 @@
+/**
+ * Buffer pool class
+ * @author      Pranav Prabhu
+ * @version     06/06/2023
+ */
+
+import java.io.File;
+
+public class BufferPool {
+    private int numOfBlocks;
+    private final int BLOCK_SIZE = 4096;
+    
+    /**
+     * BufferPool class constructor
+     */
+    public BufferPool(File file, int numOfBuffers) {
+        // TODO Auto-generated constructor stub
+    }
+}
