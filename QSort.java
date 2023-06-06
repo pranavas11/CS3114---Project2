ee3dd04ba28de509c5f14354b7951a8c020b5c0c
Pranav Prabhu <ppranavas@gmail.com>
2023-06-06 18:12:35 -0400

    Update Version 1

--- QSort.java
+++ QSort.java
@@ -1,33 +1,47 @@
 /**
- * 
+ * Implementation of Quicksort on the buffered pool.
+ * @author      Pranav Prabhu
+ * @version     06/06/2023
  */
 
-/**
- * @author pranavas11
- *
- */
+import java.nio.ByteBuffer;
+import java.io.IOException;
+import java.io.File;
+
 public class QSort {
+    private BufferPool buffer;
+    private int diskSize;
+    private int numOfBuffers;
+    private final int BLOCK_SIZE = 4096;
+    
     /**
-     * 
+     * QSort class constructor
      */
-    public QSort() {
+    public QSort(String fileName, int numBuffers) {
         // parameter: buffer pool
-        // TODO Auto-generated constructor stub
+        File file = new File(fileName);
+        this.buffer = new BufferPool(file, numBuffers);
+        this.numOfBuffers = numBuffers;
     }
     
     int partition(Comparable[] A, int left, int right, Comparable pivot) {
-        while (left <= right) { // Move bounds inward until they meet
-          while (A[left].compareTo(pivot) < 0) left++;
-          while ((right >= left) && (A[right].compareTo(pivot) >= 0)) right--;
-          if (right > left) swap(A, left, right); // Swap out-of-place values
+        // move bounds inward until they meet
+        while (left <= right) {
+            while (A[left].compareTo(pivot) < 0) left++;
+            while ((right >= left) && (A[right].compareTo(pivot) >= 0)) right--;
+            if (right > left) swap(A, left, right); // swap out-of-place values
         }
-        return left;            // Return first position in right partition
+        return left;    // return first position in right partition
       }
 
-      int findpivot(Comparable[] A, int i, int j) { return (i+j)/2; }
+      int findpivot(Comparable[] A, int i, int j) {
+          return (i + j) / 2;
+      }
       
       public void swap(Comparable[] A, int i, int j) {
-          // implememnt swap
+          Comparable temp = A[i];
+          A[i] = A[j];
+          A[j] = temp;
       }
       
       //public
@@ -36,17 +50,21 @@ public class QSort {
        *        - private short key (key comes from byte buffer; takes a byte array record and you wrap that record using wrap() builtin method in byte buffer. Then call the method getShort() which returns a key.)
        * 
        * 2. boolean checkDuplicates: returns true if all key values from positions from i to j are equal to the given pivot
-       * 3. 
        */
-      
 
-      void quicksort(Comparable[] A, int i, int j) { // Quicksort
-        int pivotindex = findpivot(A, i, j);  // Pick a pivot
-        swap(A, pivotindex, j);               // Stick pivot at end
-        // k will be the first position in the right subarray
-        int k = partition(A, i, j-1, A[j]);
-        swap(A, k, j);                        // Put pivot in place
-        if ((k-i) > 1) quicksort(A, i, k-1);  // Sort left partition
-        if ((j-k) > 1) quicksort(A, k+1, j);  // Sort right partition
+      void quicksort(Comparable[] A, int i, int j) {
+          int pivotindex = findpivot(A, i, j);      // pick a pivot
+          swap(A, pivotindex, j);                   // stick pivot at end
+          
+          // k will be the first position in the right subarray
+          int k = partition(A, i, j-1, A[j]);
+          swap(A, k, j);                            // put pivot in place
+          if ((k-i) > 1) quicksort(A, i, k-1);      // sort left partition
+          if ((j-k) > 1) quicksort(A, k+1, j);      // sort right partition
+      }
+      
+      public short getKey(byte[] record) {
+          ByteBuffer buffer = ByteBuffer.wrap(record);
+          return buffer.getShort();
       }
 }
--- Quicksort.java
+++ Quicksort.java
@@ -1,11 +1,29 @@
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class Quicksort {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner input = null;
+        
         if (args.length != 3) {
-            System.out.println("Error: No input file provided! Exiting...");
+            System.out.println("Error: Please provide the data file name, number of buffers, and stat file as arguments.");
             return;
         }
-        System.out.println("Hello World");
+        
+        try {
+            input = new Scanner(new File(args[0]));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred. No such file exists. Please try again!");
+            return;
+        }
+        
+        String dataFile = args[0];
+        long numOfBuffers = Integer.parseInt(args[1]);
+        String statFile = args[2];
     }
 }
